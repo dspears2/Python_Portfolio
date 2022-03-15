bc9c1d22bae3796595606fbef4601104c98f50d3
DeMarco Spears <demarcospears22@gmail.com>
2022-03-15 13:46:52 -0500

    Initial Commit for new python portfolio

---
+++ home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse("This is the data")
+
+def paths(request):
+    #displays all paths on the website
+    #fetch all the data from the models
+    
+    context ={
+        'heading': "django tutorial 1",
+        'content': "this is the best tutorial on the plant"
+    }
+    return HttpResponse("This is a path view")
