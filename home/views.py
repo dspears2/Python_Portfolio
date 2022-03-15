037e37d4fb47bc70a8279249a42d099783a1ac4c
DeMarco Spears <demarcospears22@gmail.com>
2022-03-15 13:55:20 -0500

    updated

--- home/views.py
+++ home/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
 def home(request):
     return HttpResponse("This is the data")
 
+
 def paths(request):
-    #displays all paths on the website
-    #fetch all the data from the models
-    
-    context ={
+    # displays all paths on the website
+    # fetch all the data from the models
+
+    context = {
         'heading': "django tutorial 1",
         'content': "this is the best tutorial on the plant"
     }
